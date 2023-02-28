860d247684f26fbe9bf78ca979bb5036ce8ee0e2
Jorge <alfred.jorge@gmail.com>
2023-02-28 18:54:28 -0300

    Initial Commit

---
+++ demo/send_email.py
@@ -0,0 +1,24 @@
+from email.mime.text import MIMEText
+import smtplib
+
+
+def send_email(email, height, average_height, count):
+    from_email = "YOUR_EMAIL"
+    from_password = "EMAIL_APP_PASSWORD"
+    to_email = email
+
+    subject = "Height Data"
+    message = "Hey there, your height is <strong>%s</strong>. " \
+            "Average height of all is <strong>%s</strong> and that " \
+              "is calculated out <strong>%s</strong> of people." % (height, average_height, count)
+
+    msg= MIMEText(message, 'html')
+    msg['Subject'] = subject
+    msg['To'] = to_email
+    msg['From'] = from_email
+
+    gmail = smtplib.SMTP('smtp.gmail.com', 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(from_email, from_password)
+    gmail.send_message(msg)
